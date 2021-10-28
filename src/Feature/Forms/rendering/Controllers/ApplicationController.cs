85dcd64a951bfc3f7e2d0acec9470b2d3d774f8b
Mohammed Syam <mohammed.syam@americaneagle.com>
2021-10-28 13:41:07 -0500

    Initial code push for mvp application steps 2 - 7

--- src/Feature/Forms/rendering/Controllers/ApplicationController.cs
+++ src/Feature/Forms/rendering/Controllers/ApplicationController.cs
@@ -16,6 +16,7 @@ using System.Net.Http;
 using System.Security.Claims;
 using System.Text;
 using static Mvp.Feature.Forms.Constants;
+using static Mvp.Feature.Forms.Models.ApplicationEditableFields;
 
 namespace Mvp.Feature.Forms.Controllers
 {
@@ -125,40 +126,10 @@ namespace Mvp.Feature.Forms.Controllers
                 // TODO :: Backend validation - [make sure Terms checkbox checked].
 
                 // Login into Sitecore to authenticate next operation --> create Item 
-                var authData = new Authentication()
-                {
-                    Domain = _configuration.GetValue<string>("Sitecore:Domain"),
-                    Username = _configuration.GetValue<string>("Sitecore:UserName"),
-                    Password = _configuration.GetValue<string>("Sitecore:Password")
-                };
-
-                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
-
-                var AuthUrl = $"{sitecoreUri}{SSCAPIs.AuthenticationApi}";
-
-                var authRequest = (HttpWebRequest)WebRequest.Create(AuthUrl);
-                authRequest.Method = "POST";
-                authRequest.ContentType = "application/json";
-
-                var requestAuthBody = JsonConvert.SerializeObject(authData);
-                var authDatas = new UTF8Encoding().GetBytes(requestAuthBody);
-
-                using (var dataStream = authRequest.GetRequestStream())
-                {
-                    dataStream.Write(authDatas, 0, authDatas.Length);
-                }
-
-                CookieContainer cookies = new CookieContainer();
-                authRequest.CookieContainer = cookies;
-
-                var authResponse = authRequest.GetResponse();
-
-                _logger.LogDebug($"Login Status:\n\r{((HttpWebResponse)authResponse).StatusDescription}");
-
-                authResponse.Close();
+                var cookies = Authenticate();
 
                 // Use SSC to create application Item
-
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
                 var user = HttpContext.User;
                 var identity = (ClaimsIdentity)user?.Identity;
                 var createApplication = new CreateApplication
@@ -170,7 +141,7 @@ namespace Mvp.Feature.Forms.Controllers
                 };
 
                 //TODO - how can we get the path to the user item to pass into the item service API
-                var createItemUrl = $"{sitecoreUri}{SSCAPIs.CreateItemApi}{"sitecore%2Fcontent%2FMvpSite%2FMVP%20Repository%2FPeople%2Fa%2FAlex%20Lentz?database=master"}"; // Path need to be added to th end ex: https://%3Cdomain%3E/sitecore/api/ssc/item/sitecore%2Fcontent%2Fhome
+                var createItemUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{"sitecore%2Fcontent%2FMvpSite%2FMVP%20Repository%2FPeople%2Fa%2FAlex%20Lentz?database=master"}"; // Path need to be added to th end ex: https://%3Cdomain%3E/sitecore/api/ssc/item/sitecore%2Fcontent%2Fhome
                 var request = (HttpWebRequest)WebRequest.Create(createItemUrl);
 
                 request.Method = "POST";
@@ -194,7 +165,7 @@ namespace Mvp.Feature.Forms.Controllers
                 if (((HttpWebResponse)response).StatusCode == HttpStatusCode.Created)
                 {
                     var createdItemId = response.Headers["Location"].Substring(response.Headers["Location"].LastIndexOf("/"), response.Headers["Location"].Length - response.Headers["Location"].LastIndexOf("/")).Split("?")[0].TrimStart('/');
-                    HttpContext.Session.SetString("UserApplicationId", createdItemId);
+                    HttpContext.Session.SetString(SessionConstants.UserApplicationId, createdItemId);
                     return Json(new { success = true, responseText = "Application succesffuly created." });
                 }
             }
@@ -204,6 +175,313 @@ namespace Mvp.Feature.Forms.Controllers
             }
             return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
 
+        }
+
+
+        [HttpGet("getcategories")]
+        public IActionResult GetCategories()
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var categories = $"{Constants.Paths.Categories}";
+                var getItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}?path={categories}&database=master&language=en&includeStandardTemplateFields=true&includeMetadata=true";
+
+                var cookies = Authenticate();
+
+                var request = (HttpWebRequest)WebRequest.Create(getItemByPathUrl);
+
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var response = request.GetResponse();
+                var JsonData = string.Empty;
+                using (var twitpicResponse = (HttpWebResponse)response)
+                {
+                    using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
+                    {
+                        JsonData = reader.ReadToEnd();
+                    }
+                }
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = JsonData });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Category(string category)
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var updateItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{HttpContext.Session.GetString(SessionConstants.UserApplicationId)}/?database=master&language=en";
+
+                var cookies = Authenticate();
+
+                var categoryModel = new CategoryModel
+                {
+                    Category = category
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(updateItemByPathUrl);
+
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var requestBody = JsonConvert.SerializeObject(categoryModel);
+
+                var data = new UTF8Encoding().GetBytes(requestBody);
+
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(data, 0, data.Length);
+                }
+
+                var response = request.GetResponse();
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = "Application succesffuly created." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult PersonalInformation(string firstName, string lastName, string preferredName, string employmentStatus, 
+            string companyName, string country, string state, string mentor)
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var updateItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{HttpContext.Session.GetString(SessionConstants.UserApplicationId)}/?database=master&language=en";
+
+                var cookies = Authenticate();
+
+                var personalInformationModel = new PersonalInformationModel
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PreferredName = preferredName,
+                    EmploymentStatus = employmentStatus,
+                    CompanyName = companyName,
+                    Country = country,
+                    State = state, 
+                    Mentor = mentor
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(updateItemByPathUrl);
+
+                request.Method = "PATCH";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var requestBody = JsonConvert.SerializeObject(personalInformationModel);
+
+                var data = new UTF8Encoding().GetBytes(requestBody);
+
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(data, 0, data.Length);
+                }
+
+                var response = request.GetResponse();
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = "Application succesffuly created." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ObjectivesandMotivation(string eligibility, string objectives)
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var updateItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{HttpContext.Session.GetString(SessionConstants.UserApplicationId)}/?database=master&language=en";
+
+                var cookies = Authenticate();
+
+                var objectivesandMotivationModel = new ObjectivesandMotivationModel
+                {
+                    Eligibility= eligibility,
+                    Objectives = objectives
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(updateItemByPathUrl);
+
+                request.Method = "PATCH";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var requestBody = JsonConvert.SerializeObject(objectivesandMotivationModel);
+
+                var data = new UTF8Encoding().GetBytes(requestBody);
+
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(data, 0, data.Length);
+                }
+
+                var response = request.GetResponse();
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = "Application succesffuly created." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Socials(string blog, string sitecoreCommunity, string customerCoreProfile, string stackExchange, string gitHub, string twitter, string others)
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var updateItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{HttpContext.Session.GetString(SessionConstants.UserApplicationId)}/?database=master&language=en";
+
+                var cookies = Authenticate();
+
+                var communityProfilesModel = new CommunityProfilesModel
+                {
+                    Blog = blog,
+                    SitecoreCommunity = sitecoreCommunity,
+                    CustomerCoreProfile = customerCoreProfile,
+                    StackExchange = stackExchange,
+                    GitHub= gitHub,
+                    Twitter = twitter
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(updateItemByPathUrl);
+
+                request.Method = "PATCH";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var requestBody = JsonConvert.SerializeObject(communityProfilesModel);
+
+                var data = new UTF8Encoding().GetBytes(requestBody);
+
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(data, 0, data.Length);
+                }
+
+                var response = request.GetResponse();
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = "Application succesffuly created." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult NotableCurrentYearContributions(string onlineAcvitity, string offlineActivity)
+        {
+            try
+            {
+                var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+                var updateItemByPathUrl = $"{sitecoreUri}{SSCAPIs.ItemApi}{HttpContext.Session.GetString(SessionConstants.UserApplicationId)}/?database=master&language=en";
+
+                var cookies = Authenticate();
+
+                var NotableCurrentYearContributionsModeln= new NotableCurrentYearContributionsModel
+                {
+                    OnlineAcvitity = onlineAcvitity,
+                    OfflineActivity = offlineActivity
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(updateItemByPathUrl);
+
+                request.Method = "PATCH";
+                request.ContentType = "application/json";
+                request.CookieContainer = cookies;
+
+                var requestBody = JsonConvert.SerializeObject(NotableCurrentYearContributionsModeln);
+
+                var data = new UTF8Encoding().GetBytes(requestBody);
+
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(data, 0, data.Length);
+                }
+
+                var response = request.GetResponse();
+
+                _logger.LogDebug($"Item Status:\n\r{((HttpWebResponse)response).StatusDescription}");
+
+                return Json(new { success = true, responseText = "Application succesffuly created." });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message, null);
+                return Json(new { success = false, responseText = "An error occured while creating your application.  Please contact Sitecore Support." });
+            }
+        }
+
+        private CookieContainer Authenticate()
+        {
+            var authData = new Authentication()
+            {
+                Domain = _configuration.GetValue<string>("Sitecore:Domain"),
+                Username = _configuration.GetValue<string>("Sitecore:UserName"),
+                Password = _configuration.GetValue<string>("Sitecore:Password")
+            };
+
+            var sitecoreUri = _configuration.GetValue<string>("Sitecore:InstanceCMUri");
+
+            var AuthUrl = $"{sitecoreUri}{SSCAPIs.AuthenticationApi}";
+
+            var authRequest = (HttpWebRequest)WebRequest.Create(AuthUrl);
+            authRequest.Method = "POST";
+            authRequest.ContentType = "application/json";
+
+            var requestAuthBody = JsonConvert.SerializeObject(authData);
+            var authDatas = new UTF8Encoding().GetBytes(requestAuthBody);
+
+            using (var dataStream = authRequest.GetRequestStream())
+            {
+                dataStream.Write(authDatas, 0, authDatas.Length);
+            }
+
+            CookieContainer cookies = new CookieContainer();
+            authRequest.CookieContainer = cookies;
+
+            var authResponse = authRequest.GetResponse();
+
+            _logger.LogDebug($"Login Status:\n\r{((HttpWebResponse)authResponse).StatusDescription}");
+
+            authResponse.Close();
+
+
+            return cookies;
         }
     }
 }
--- src/Project/MvpSite/rendering/wwwroot/js/mvp-site.js
+++ src/Project/MvpSite/rendering/wwwroot/js/mvp-site.js
@@ -53,7 +53,239 @@
                     form.classList.add('was-validated')
                 }, false)
             })
-    });		
+    });
+
+    $("#btnStep2").click(function (event) {
+        'use strict'
+        var forms = document.querySelectorAll('#categoryForm')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    if (!form.checkValidity()) {
+                        event.stopPropagation()
+                    }
+                    else {
+                        // get data from the form
+                        var _category = $("#dllcategory").find("option:selected").text();
+
+                        $.ajax({
+                            url: '/Application/Category',
+                            type: 'post',
+                            data: JSON.stringify({ category: _category }),
+                            dataType: 'json',
+                            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                            success: function (data) {
+                                if (data.success === true) {
+                                    setStep('#step_personal', 3);
+                                }
+                                else {
+                                    alert(data.responseText);
+                                }
+
+                            }
+                        }).done(function () {
+                            setTimeout(function () {
+                                $("#overlay").fadeOut(300);
+                            }, 500);
+                        });
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
+
+    $("#btnStep3").click(function (event) {
+        'use strict'
+        var forms = document.querySelectorAll('#personalForm')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    if (!form.checkValidity()) {
+                        event.stopPropagation()
+                    }
+                    else {
+                        // get data from the form
+                        var _firstName = $('#firstName').val();
+                        var _lastName = $('#lastName').val();
+                        var _preferredName = $('#preferredName').val();
+                        var _employmentStatus = $("#ddlEmploymentStatus").find("option:selected").text();
+                        var _companyName = $('#companyName').val();
+                        var _country = $("#dllCountry").find("option:selected").text();
+                        var _mentor = $('#mentor').val();
+
+
+                        $.ajax({
+                            url: '/Application/PersonalInformation',
+                            type: 'post',
+                            data: JSON.stringify({ firstName: _firstName, lastName: _lastName, preferredName: _preferredName, employmentStatus: _employmentStatus, companyName: _companyName, country: _country, state: '', mentor: _mentor }),
+                            dataType: 'json',
+                            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                            success: function (data) {
+                                if (data.success === true) {
+                                    setStep('#step_objectives', 4);
+                                }
+                                else {
+                                    alert(data.responseText);
+                                }
+
+                            }
+                        }).done(function () {
+                            setTimeout(function () {
+                                $("#overlay").fadeOut(300);
+                            }, 500);
+                        });
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
+
+    $("#btnStep4").click(function (event) {
+        'use strict'
+        var forms = document.querySelectorAll('#objectivesForm')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    if (!form.checkValidity()) {
+                        event.stopPropagation()
+                    }
+                    else {
+                        // get data from the form
+                        var _eligibility = $('#eligibility').val();
+                        var _objectives = $('#objectives').val();
+
+
+                        $.ajax({
+                            url: '/Application/ObjectivesandMotivation',
+                            type: 'post',
+                            data: JSON.stringify({ eligibility: _eligibility, objectives: _objectives}),
+                            dataType: 'json',
+                            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                            success: function (data) {
+                                if (data.success === true) {
+                                    setStep('#step_socials', 5);
+                                }
+                                else {
+                                    alert(data.responseText);
+                                }
+
+                            }
+                        }).done(function () {
+                            setTimeout(function () {
+                                $("#overlay").fadeOut(300);
+                            }, 500);
+                        });
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
+
+    $("#btnStep5").click(function (event) {
+        'use strict'
+        var forms = document.querySelectorAll('#socialForm')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    if (!form.checkValidity()) {
+                        event.stopPropagation()
+                    }
+                    else {
+                        // get data from the form
+                        var _blog = $('#blog').val();
+                        var _sitecoreCommunity = $('#customerCoreProfile').val();
+                        var _customerCoreProfile = $('#customerCoreProfile').val();
+                        var _stackExchange = $('#stackExchange').val();
+                        var _gitHub = $('#gitHub').val();
+                        var _twitter = $('#twitter').val();
+                        var _others = $('#others').val();
+
+
+                        $.ajax({
+                            url: '/Application/Socials',
+                            type: 'post',
+                            data: JSON.stringify({ blog: _blog, sitecoreCommunity: _sitecoreCommunity, customerCoreProfile: _customerCoreProfile, stackExchange: _stackExchange, gitHub: _gitHub, twitter: _twitter, others: _others }),
+                            dataType: 'json',
+                            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                            success: function (data) {
+                                if (data.success === true) {
+                                    setStep('#step_contributions', 6);
+                                }
+                                else {
+                                    alert(data.responseText);
+                                }
+
+                            }
+                        }).done(function () {
+                            setTimeout(function () {
+                                $("#overlay").fadeOut(300);
+                            }, 500);
+                        });
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
+
+    $("#btnStep6").click(function (event) {
+        'use strict'
+        var forms = document.querySelectorAll('#contributionForm')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault()
+                    if (!form.checkValidity()) {
+                        event.stopPropagation()
+                    }
+                    else {
+                        // get data from the form
+                        var _onlineAcvitity = $('#onlineAcvitity').val();
+                        var _offlineActivity = $('#offlineActivity').val();
+
+                        $.ajax({
+                            url: '/Application/NotableCurrentYearContributions',
+                            type: 'post',
+                            data: JSON.stringify({ onlineAcvitity: _onlineAcvitity, offlineActivity: _offlineActivity, }),
+                            dataType: 'json',
+                            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                            success: function (data) {
+                                if (data.success === true) {
+                                    setStep('#step_confirmation', 7);
+                                }
+                                else {
+                                    alert(data.responseText);
+                                }
+
+                            }
+                        }).done(function () {
+                            setTimeout(function () {
+                                $("#overlay").fadeOut(300);
+                            }, 500);
+                        });
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    });
 
     function setStep(stepId, stepCount) {
         //hide all steps
