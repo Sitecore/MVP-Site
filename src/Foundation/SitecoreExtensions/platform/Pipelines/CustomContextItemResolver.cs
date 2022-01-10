2d2fc10abdde127f0e6f039361fab187d3a5e870
Sebastian Tecsi <sebastiantecsi@users.noreply.github.com>
2022-01-11 00:23:08 +0200

    Rendering host to resolve aliases (#159)

---
+++ src/Foundation/SitecoreExtensions/platform/Pipelines/CustomContextItemResolver.cs
@@ -0,0 +1,58 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Mvc.ItemResolving;
+using Sitecore.LayoutService.Mvc.Pipelines.RequestBegin;
+using Sitecore.LayoutService.Mvc.Routing;
+using Sitecore.Mvc.Pipelines.Request.RequestBegin;
+
+namespace Mvp.Foundation.SitecoreExtensions.Pipelines
+{
+    public class CustomContextItemResolver : ContextItemResolver
+    {
+        public CustomContextItemResolver(IItemResolver itemResolver, IRouteMapper routeMapper) : base(itemResolver, routeMapper)
+        {
+        }
+
+        public override void Process(RequestBeginArgs args)
+        {
+            base.Process(args);
+            if (!Settings.AliasesActive)
+            {
+                return;
+                // if aliases aren't active, we really shouldn't confuse whoever turned them off 
+            }
+
+            var database = Context.Database;
+            if (database == null)
+            {
+                return;
+                // similarly, if we don't have a database, we probably shouldn't try to do anything
+            }
+
+            if (Context.Item == null)
+            {
+                string path = this.GetPath(args);
+                if (string.IsNullOrEmpty(path))
+                {
+                    return;
+                }
+
+                ID targetID = database.Aliases.GetTargetID(path);
+                if (targetID.IsNull)
+                {
+                    string message = string.Concat("An alias for \"", path, "\" exists, but points to a non-existing item.");
+                    Log.Info(message, this);
+                    return;
+                }
+                Item item = database.GetItem(targetID);
+                if (item != null)
+                {
+                    Context.Item = item;
+                }
+            }
+        }
+    }
+}
