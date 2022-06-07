f73ccef46db1664aff515842760f87532890deff
Joseph Long <joseph.long@sitecore.com>
2022-06-07 19:46:59 +0100

    Added healthcheck.

---
+++ src/Environment/platform/Healthcheck/ApplicationInitializationCheck.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Sitecore.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Mvp.Environment.Sitecore.Healthcheck
+{
+    public class ApplicationInitializationCheck : IHealthCheck
+    {
+        public ApplicationInitializationCheck() { }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var machineName = "ContentDelivery"; /*Environment.GetEnvironmentVariable("Sitecore_InstanceName") ?? Environment.MachineName;*/
+            if ((string)HttpContext.Current.Cache.Get("APPINIT") == "1")
+            {
+                Log.Warn($"Warmup is in progress of {machineName}", this);
+                return HealthCheckResult.Unhealthy($"Warmup is in progress of {machineName}");
+            }
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ src/Environment/platform/Sitecore/custom/appinit.aspx
@@ -0,0 +1,11 @@
+<%@ Page Language="C#" AutoEventWireup="true" %>
+<script runat="server">
+    void Page_Load()
+    {
+        if (HttpContext.Current.Request.QueryString.Count > 0)
+            HttpContext.Current.Cache.Insert("APPINIT", "1", null, DateTime.Now.AddSeconds(600), TimeSpan.Zero);
+        else
+            HttpContext.Current.Cache.Remove("APPINIT");
+    }
+</script>
+OK
