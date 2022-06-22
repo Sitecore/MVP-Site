79a7c7069bf4b19882520a7a1b4f0a0cf4726da3
Joseph Long <joseph.long@sitecore.com>
2022-06-22 12:46:48 +0100

    Removed tostring.

--- src/Environment/platform/Healthcheck/ApplicationInitializationCheck.cs
+++ src/Environment/platform/Healthcheck/ApplicationInitializationCheck.cs
@@ -14,7 +14,7 @@ namespace Mvp.Environment.Sitecore.Healthcheck
         {
             var machineName = "CM"; /*Environment.GetEnvironmentVariable("Sitecore_InstanceName") ?? Environment.MachineName;*/
 
-            if (HttpContext.Current.Cache.Get("APPINIT").ToString() == "1")
+            if ((string)HttpContext.Current.Cache.Get("APPINIT") == "1")
             {
                 return HealthCheckResult.Unhealthy($"Warmup is in progress for {machineName}");
             }
